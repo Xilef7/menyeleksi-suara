77c568a27f7408cea3b8112318a58586d44f79ee
Felix Septianus Darmawan <13516041@std.stei.itb.ac.id>
2019-03-25 10:10:21 +0700

    Split responses report to multiple lines

--- Assets/Scripts/GameReporter.cs
+++ Assets/Scripts/GameReporter.cs
@@ -2,27 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using UnityEngine;
 
 public class GameReporter : MonoBehaviour
 {
-    public class LevelScore
-    {
-        public int CountHit;
-        public int CountPremature;
-        public int CountCommission;
-        public int CountOmission;
-
-        public LevelScore(int countHit, int countPremature, int countCommission, int countOmission)
-        {
-            CountHit = countHit;
-            CountPremature = countPremature;
-            CountCommission = countCommission;
-            CountOmission = countOmission;
-        }
-    }
-
     public class Score
     {
         public LevelScore[] LevelScores;
@@ -88,26 +71,20 @@ public class GameReporter : MonoBehaviour
             }
         }
     }
-
-    private enum Accuracy
+    
+    public class LevelScore
     {
-        Hit,
-        Premature,
-        Commission,
-        Omission
-    }
+        public int CountHit;
+        public int CountPremature;
+        public int CountCommission;
+        public int CountOmission;
 
-    private class ResponseDatum
-    {
-        public int StimulusIndex;
-        public Accuracy Accuracy;
-        public int ResponseCount;
-
-        public ResponseDatum(int stimulusIndex, Accuracy accuracy)
+        public LevelScore(int countHit, int countPremature, int countCommission, int countOmission)
         {
-            StimulusIndex = stimulusIndex;
-            Accuracy = accuracy;
-            ResponseCount = 1;
+            CountHit = countHit;
+            CountPremature = countPremature;
+            CountCommission = countCommission;
+            CountOmission = countOmission;
         }
     }
 
@@ -118,18 +95,9 @@ public class GameReporter : MonoBehaviour
 
     private GameManager gameManager;
 
-    private ColorStreamOne[] stream;
-
-    private float[] sumHitResponseTime;
-    private float[] sumPrematureResponseTime;
-    private float[] sumCommissionResponseTime;
-
-    private int[] countHit;
-    private int[] countPremature;
-    private int[] countCommission;
-    private int[] countOmission;
-
-    private LinkedList<ResponseDatum> responseData = null;
+    private ReportWriter reportWriter;
+    private LinkedList<ResponseOfStimulus> responseOfStimuli;
+    private ResponseStatistic responseStatistic;
 
     private int totalLevel;
 
@@ -158,74 +126,61 @@ public class GameReporter : MonoBehaviour
 
     void OnGameStarted(ReadOnlyCollection<ColorStreamOne> stream)
     {
-        this.stream = new ColorStreamOne[stream.Count];
-        stream.CopyTo(this.stream, 0);
-        sumHitResponseTime = new float[totalLevel];
-        sumPrematureResponseTime = new float[totalLevel];
-        sumCommissionResponseTime = new float[totalLevel];
-        countHit = new int[totalLevel];
-        countPremature = new int[totalLevel];
-        countCommission = new int[totalLevel];
-        countOmission = new int[totalLevel];
-        responseData = new LinkedList<ResponseDatum>();
+        responseOfStimuli = new LinkedList<ResponseOfStimulus>();
+        responseStatistic = new ResponseStatistic(totalLevel);
+        reportWriter = new ReportWriter(stream, responseOfStimuli, responseStatistic);
     }
 
     void OnHit(ResponseInfo responseInfo)
     {
-        int i = responseInfo.CurrentLevel - 1;
         if (!responseInfo.HasRespondedBefore)
         {
-            sumHitResponseTime[i] += responseInfo.ResponseTime;
-            countHit[i]++;
-            responseData.AddLast(new ResponseDatum(responseInfo.RespondedStimulusIndex, Accuracy.Hit));
+            responseStatistic.SetHit(responseInfo.ResponseTime, responseInfo.CurrentLevel);
+            responseOfStimuli.AddLast(new ResponseOfStimulus(responseInfo.RespondedStimulusIndex, Accuracy.Hit));
         }
         else
         {
-            responseData.Last.Value.ResponseCount++;
+            responseOfStimuli.Last.Value.ResponseCount++;
         }
     }
 
     void OnPremature(ResponseInfo responseInfo)
     {
-        int i = responseInfo.CurrentLevel - 1;
         if (!responseInfo.HasRespondedBefore)
         {
-            sumPrematureResponseTime[i] += responseInfo.ResponseTime;
-            countPremature[i]++;
-            responseData.AddLast(new ResponseDatum(responseInfo.RespondedStimulusIndex, Accuracy.Premature));
+            responseStatistic.SetPremature(responseInfo.ResponseTime, responseInfo.CurrentLevel);
+            responseOfStimuli.AddLast(new ResponseOfStimulus(responseInfo.RespondedStimulusIndex, Accuracy.Premature));
         }
         else
         {
-            responseData.Last.Value.ResponseCount++;
+            responseOfStimuli.Last.Value.ResponseCount++;
         }
     }
 
     void OnCommission(ResponseInfo responseInfo)
     {
-        int i = responseInfo.CurrentLevel - 1;
         if (!responseInfo.HasRespondedBefore)
         {
-            sumCommissionResponseTime[i] += responseInfo.ResponseTime;
-            countCommission[i]++;
-            responseData.AddLast(new ResponseDatum(responseInfo.RespondedStimulusIndex, Accuracy.Commission));
+            responseStatistic.SetCommision(responseInfo.ResponseTime, responseInfo.CurrentLevel);
+            responseOfStimuli.AddLast(new ResponseOfStimulus(responseInfo.RespondedStimulusIndex, Accuracy.Commission));
         }
         else
         {
-            responseData.Last.Value.ResponseCount++;
+            responseOfStimuli.Last.Value.ResponseCount++;
         }
     }
 
     void OnOmission(int currentLevel, int lastStimulusIndex)
     {
-        countOmission[currentLevel - 1]++;
-        responseData.AddLast(new ResponseDatum(lastStimulusIndex, Accuracy.Omission));
+        responseStatistic.SetOmission(currentLevel);
+        responseOfStimuli.AddLast(new ResponseOfStimulus(lastStimulusIndex, Accuracy.Omission));
     }
 
     void OnGameEnded()
     {
         if (shouldRecord)
         {
-            Report();
+            reportWriter.WriteReport("report.txt", 66);
         }
 
         StartCoroutine(WaitThenShowScoreCalculation());
@@ -234,80 +189,21 @@ public class GameReporter : MonoBehaviour
     IEnumerator WaitThenShowScoreCalculation()
     {
         yield return new WaitForSeconds(waitDuration);
+
+        int[] countHit = new int[totalLevel];
+        int[] countPremature = new int[totalLevel];
+        int[] countCommission = new int[totalLevel];
+        int[] countOmission = new int[totalLevel];
+
+        for (int i = 0; i < totalLevel; i++)
+        {
+            int level = i + 1;
+            countHit[i] = responseStatistic.GetHitCount(level);
+            countPremature[i] = responseStatistic.GetPrematureCount(level);
+            countCommission[i] = responseStatistic.GetCommissionCount(level);
+            countOmission[i] = responseStatistic.GetOmissionCount(level);
+        }
+
         ScoreCalculated(new Score(countHit, countPremature, countCommission, countOmission));
-    }
-
-    void Report()
-    {
-        using (StreamWriter writer = new StreamWriter("report.txt", true, System.Text.Encoding.Unicode))
-        {
-            writer.WriteLine("********************************************************************");
-
-            writer.WriteLine(System.DateTime.Now.ToString("F", new System.Globalization.CultureInfo("id-ID")));
-            writer.WriteLine();
-
-            writer.WriteLine("Response Count:");
-            for (int i = 0; i < totalLevel; i++)
-            {
-                writer.WriteLine(
-                    "Level {0}: [ Hit: {1,-3:D} Premature: {2,-3:D} Commission: {3,-3:D} Omission: {4,-3:D} ]",
-                    i + 1,
-                    countHit[i],
-                    countPremature[i],
-                    countCommission[i],
-                    countOmission[i]);
-            }
-            writer.WriteLine();
-
-            writer.WriteLine("Response Time:");
-            for (int i = 0; i < totalLevel; i++)
-            {
-                writer.WriteLine(
-                    "Level {0}: [ Hit: {1,-7:G5}s Premature: {2,-7:G5}s Commission: {3,-7:G5}s ]",
-                    i + 1,
-                    sumHitResponseTime[i] / countHit[i],
-                    sumPrematureResponseTime[i] / countPremature[i],
-                    sumCommissionResponseTime[i] / countCommission[i]);
-            }
-            writer.WriteLine();
-
-            writer.Write("S: ");
-            foreach (var color in stream)
-            {
-                writer.Write("{0} ", color == ColorStreamOne.Black ? 'B' : 'R');
-            }
-            writer.WriteLine();
-
-            writer.Write("H: ");
-            WriteResponse(writer, Accuracy.Hit);
-            writer.Write("P: ");
-            WriteResponse(writer, Accuracy.Premature);
-            writer.Write("C: ");
-            WriteResponse(writer, Accuracy.Commission);
-            writer.Write("O: ");
-            WriteResponse(writer, Accuracy.Omission);
-
-            writer.WriteLine("********************************************************************");
-            writer.WriteLine();
-        }
-    }
-
-    void WriteResponse(StreamWriter writer, Accuracy accuracy)
-    {
-        int i = 0;
-        foreach (var responseDatum in responseData)
-        {
-            if (responseDatum.Accuracy == accuracy)
-            {
-                for (; i < responseDatum.StimulusIndex; i++)
-                {
-                    writer.Write("  ");
-                }
-                writer.Write(responseDatum.ResponseCount);
-                writer.Write(' ');
-                i++;
-            }
-        }
-        writer.WriteLine();
     }
 }
---
+++ Assets/Scripts/ReportWriter.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+public enum Accuracy
+{
+    Hit,
+    Premature,
+    Commission,
+    Omission
+}
+
+public class ResponseStatistic
+{
+    public int TotalLevel
+    {
+        get;
+    }
+
+    private float[] sumHitResponseTime;
+    private float[] sumPrematureResponseTime;
+    private float[] sumCommissionResponseTime;
+
+    private int[] countHit;
+    private int[] countPremature;
+    private int[] countCommission;
+    private int[] countOmission;
+
+    public ResponseStatistic(int totalLevel)
+    {
+        TotalLevel = totalLevel;
+        sumHitResponseTime = new float[totalLevel];
+        sumPrematureResponseTime = new float[totalLevel];
+        sumCommissionResponseTime = new float[totalLevel];
+        countHit = new int[totalLevel];
+        countPremature = new int[totalLevel];
+        countCommission = new int[totalLevel];
+        countOmission = new int[totalLevel];
+    }
+
+    public void SetHit(float responseTime, int level)
+    {
+        int i = level - 1;
+        sumHitResponseTime[i] += responseTime;
+        countHit[i]++;
+    }
+
+    public void SetPremature(float responseTime, int level)
+    {
+        int i = level - 1;
+        sumPrematureResponseTime[i] += responseTime;
+        countPremature[i]++;
+    }
+
+    public void SetCommision(float responseTime, int level)
+    {
+        int i = level - 1;
+        sumCommissionResponseTime[i] += responseTime;
+        countCommission[i]++;
+    }
+
+    public void SetOmission(int level)
+    {
+        countOmission[level - 1]++;
+    }
+
+    public int GetHitCount(int level)
+    {
+        return countHit[level - 1];
+    }
+
+    public int GetPrematureCount(int level)
+    {
+        return countPremature[level - 1];
+    }
+
+    public int GetCommissionCount(int level)
+    {
+        return countCommission[level - 1];
+    }
+
+    public int GetOmissionCount(int level)
+    {
+        return countOmission[level - 1];
+    }
+
+    public float GetAverageHitResponseTime(int level)
+    {
+        int i = level - 1;
+        return sumHitResponseTime[i] / countHit[i];
+    }
+
+    public float GetAveragePrematureResponseTime(int level)
+    {
+        int i = level - 1;
+        return sumPrematureResponseTime[i] / countPremature[i];
+    }
+
+    public float GetAverageCommissionResponseTime(int level)
+    {
+        int i = level - 1;
+        return sumCommissionResponseTime[i] / countCommission[i];
+    }
+}
+
+public class ResponseOfStimulus
+{
+    public int StimulusIndex;
+    public Accuracy Accuracy;
+    public int ResponseCount;
+
+    public ResponseOfStimulus(int stimulusIndex, Accuracy accuracy)
+    {
+        StimulusIndex = stimulusIndex;
+        Accuracy = accuracy;
+        ResponseCount = 1;
+    }
+}
+
+public class ReportWriter
+{
+    private ColorStreamOne[] stream;
+    private IEnumerable<ResponseOfStimulus> responseOfStimuli;
+    private ResponseStatistic responseStatistic;
+
+    public ReportWriter(ReadOnlyCollection<ColorStreamOne> stream, IEnumerable<ResponseOfStimulus> responseOfStimuli, ResponseStatistic responseStatistic)
+    {
+        this.stream = new ColorStreamOne[stream.Count];
+        stream.CopyTo(this.stream, 0);
+        this.responseOfStimuli = responseOfStimuli;
+        this.responseStatistic = responseStatistic;
+    }
+
+    public void WriteReport(string fileName, int width)
+    {
+        using (StreamWriter writer = new StreamWriter(fileName, true, Encoding.Unicode))
+        {
+            // Write separator
+            writer.WriteLine(new string('*', width));
+
+            // Write current date and time
+            writer.WriteLine(System.DateTime.Now.ToString("F", new System.Globalization.CultureInfo("id-ID")));
+            writer.WriteLine();
+
+            // Write response count
+            writer.WriteLine("Response Count:");
+            for (int level = 1; level <= responseStatistic.TotalLevel; level++)
+            {
+                writer.WriteLine(
+                    "Level {0}: [ Hit: {1,-3:D} Premature: {2,-3:D} Commission: {3,-3:D} Omission: {4,-3:D} ]",
+                    level,
+                    responseStatistic.GetHitCount(level),
+                    responseStatistic.GetPrematureCount(level),
+                    responseStatistic.GetCommissionCount(level),
+                    responseStatistic.GetOmissionCount(level));
+            }
+            writer.WriteLine();
+
+            // Write response time
+            writer.WriteLine("Response Time:");
+            for (int level = 1; level <= responseStatistic.TotalLevel; level++)
+            {
+                writer.WriteLine(
+                    "Level {0}: [ Hit: {1,-7:G5}s Premature: {2,-7:G5}s Commission: {3,-7:G5}s ]",
+                    level,
+                    responseStatistic.GetAverageHitResponseTime(level),
+                    responseStatistic.GetAveragePrematureResponseTime(level),
+                    responseStatistic.GetAverageCommissionResponseTime(level));
+            }
+            writer.WriteLine();
+
+            // Write response on stimulus
+            var stream = BuildStreamString('B', 'R');
+            var hitResponse = BuildResponseString(Accuracy.Hit);
+            var prematureResponse = BuildResponseString(Accuracy.Premature);
+            var commissionResponse = BuildResponseString(Accuracy.Commission);
+            var omissionResponse = BuildResponseString(Accuracy.Omission);
+
+            int partLength = width - 2;
+            for (int i = 0; i < stream.Length; i += width)
+            {
+                writer.WriteLine("S: {0}", stream.Substring(i, System.Math.Min(partLength, stream.Length - i)));
+                writer.WriteLine("H: {0}", i < hitResponse.Length ? hitResponse.Substring(i, System.Math.Min(partLength, hitResponse.Length - i)) : "");
+                writer.WriteLine("P: {0}", i < prematureResponse.Length ? prematureResponse.Substring(i, System.Math.Min(partLength, prematureResponse.Length - i)) : "");
+                writer.WriteLine("C: {0}", i < commissionResponse.Length ? commissionResponse.Substring(i, System.Math.Min(partLength, commissionResponse.Length - i)) : "");
+                writer.WriteLine("O: {0}", i < omissionResponse.Length ? omissionResponse.Substring(i, System.Math.Min(partLength, omissionResponse.Length - i)) : "");
+                writer.WriteLine();
+            }
+
+            // Write separator
+            writer.WriteLine(new string('*', width));
+            writer.WriteLine();
+        }
+    }
+
+    private string BuildStreamString(char black, char red)
+    {
+        var sb = new StringBuilder();
+        foreach (var color in stream)
+        {
+            sb.AppendFormat("{0} ", color == ColorStreamOne.Black ? black : red);
+        }
+        return sb.ToString();
+    }
+
+    private string BuildResponseString(Accuracy accuracy)
+    {
+        var sb = new StringBuilder();
+        int i = 0;
+        foreach (var responseOfStimulus in responseOfStimuli)
+        {
+            if (responseOfStimulus.Accuracy == accuracy)
+            {
+                while (i < responseOfStimulus.StimulusIndex)
+                {
+                    sb.Append(new string(' ', 2));
+                    i++;
+                }
+                sb.AppendFormat("{0} ", responseOfStimulus.ResponseCount);
+                i++;
+            }
+        }
+        return sb.ToString();
+    }
+}
