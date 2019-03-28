827349af86675269b10bd2f4b9d2a68bf7b95137
Felix Septianus Darmawan <13516041@std.stei.itb.ac.id>
2019-03-28 18:38:24 +0700

    Add double color button

---
+++ Assets/Scripts/PlayClips.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayClips : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] clips = null;
+    [SerializeField] private float delay = 2;
+
+    private AudioSource audioSource;
+    private int i;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Play()
+    {
+        i = 0;
+        InvokeRepeating("PlayCurrentClip", 0, delay);
+    }
+
+    void PlayCurrentClip()
+    {
+        audioSource.PlayOneShot(clips[i]);
+        if (++i >= clips.Length) {
+            CancelInvoke("PlayCurrentClip");
+        }
+    }
+}
